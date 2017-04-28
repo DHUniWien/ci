2121ac981ac0e1c20ad7c6b1f96d0d7c134c0965
oftl <github@rockandroll.at>
2017-04-28 18:26:08 +0200

    combine all containers in a single docker-compose file

---
+++ docker/volumes.sh
@@ -0,0 +1,7 @@
+docker volume create --name concourse-db
+docker volume create --name registry
+docker volume create --name archiva
+docker volume create --name git-server-storage
+docker volume create --name git-server-git-home-ssh
+docker volume create --name stemmarest_home-test
+docker volume create --name stemmarest_home-prod
