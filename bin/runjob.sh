a7a5204280d81d78ed7d79f10e4e30ee25750878
oftl <github@rockandroll.at>
2017-06-09 14:20:40 +0200

    add a show-option simply echoing the (longish) commands

--- bin/runjob.sh
+++ bin/runjob.sh
@@ -4,11 +4,45 @@
 #
 
 # concourse "fly" binary
-FLYBIN=
-TARGET=$1
-PIPELINE=$2
-JOB=$3
+FLYBIN=~/Downloads/fly
 
-${FLYBIN} --target ${TARGET} trigger-job -j ${PIPELINE}/${JOB}
-${FLYBIN} --target ${TARGET} watch -j ${PIPELINE}/${JOB}
-${FLYBIN} --target ${TARGET} hijack -j ${PIPELINE}/${JOB}
+function run {
+    ${FLYBIN} --target ${TARGET} trigger-job -j ${PIPELINE}/${JOB}
+    ${FLYBIN} --target ${TARGET} watch -j ${PIPELINE}/${JOB}
+    ${FLYBIN} --target ${TARGET} hijack -j ${PIPELINE}/${JOB} sh
+}
+
+function show {
+    echo "${FLYBIN} --target ${TARGET} trigger-job -j ${PIPELINE}/${JOB}"
+    echo "${FLYBIN} --target ${TARGET} watch -j ${PIPELINE}/${JOB}"
+    echo "${FLYBIN} --target ${TARGET} hijack -j ${PIPELINE}/${JOB} sh"
+}
+
+function usage {
+    echo "Usage: `basename $0` [-s] [-h]";
+    echo "-s ... show commands"
+    echo "-r ... run commands"
+    echo "-h ... this usage message"
+    echo "only one (the first) option is recognised"
+}
+
+while getopts "srh" OPT; do
+    TARGET=$2
+    PIPELINE=$3
+    JOB=$4
+
+    case $OPT in
+        s)
+            show
+        ;;
+        h)
+            usage
+        ;;
+        r)
+            run
+        ;;
+    esac
+
+    # only one of the options may be given
+    exit 0
+done
