862554f34f620802b914c2e933a1be230ecb68ae
oftl <github@rockandroll.at>
2017-08-23 16:59:27 +0200

    do not `sudo` when running docker

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,29 +13,29 @@ SSH="/usr/bin/ssh \
 
 EXTRA_OPTIONS=''
 
-INSTANCE_RUNNING=`$SSH sudo /usr/bin/docker ps --format "{{.Names}}" | grep -e "${INSTANCE_NAME}"`
+INSTANCE_RUNNING=`$SSH  /usr/bin/docker ps --format "{{.Names}}" | grep -e "${INSTANCE_NAME}"`
 if [ "${INSTANCE_RUNNING}" ] ; then
     echo "stop running instance <${INSTANCE_NAME}> ..."
-    $SSH sudo /usr/bin/docker stop ${INSTANCE_NAME}
+    $SSH  /usr/bin/docker stop ${INSTANCE_NAME}
 fi
 
-INSTANCE_EXISTS=`$SSH sudo /usr/bin/docker ps -a --format "{{.Names}}" | grep -e "${INSTANCE_NAME}"`
+INSTANCE_EXISTS=`$SSH  /usr/bin/docker ps -a --format "{{.Names}}" | grep -e "${INSTANCE_NAME}"`
 if [ "${INSTANCE_EXISTS}" ] && [ "${REMOVE_OLD_INSTANCE}" = "yes" ] ; then
     echo "remove instance <${INSTANCE_NAME}> ..."
     echo "please note, that only inline volumes will be removed with it"
-    $SSH sudo /usr/bin/docker rm ${INSTANCE_NAME}
+    $SSH  /usr/bin/docker rm ${INSTANCE_NAME}
 fi
 
 echo "pull latest of <${REGISTRY}/${IMAGE}:${TAG}>"
-$SSH sudo /usr/bin/docker pull ${REGISTRY}/${IMAGE}:${TAG}
+$SSH  /usr/bin/docker pull ${REGISTRY}/${IMAGE}:${TAG}
 
 # sh:   -n ... True if the length of string is nonzero.
 # bash: -v ... True if the shell variable varname is set (has been assigned a value).
 if [ -n "${VOLUME}" ] ; then
-    VOLUME_EXISTS=`$SSH sudo /usr/bin/docker volume ls | awk "{print $2}" | grep -e "${VOLUME}"`
+    VOLUME_EXISTS=`$SSH  /usr/bin/docker volume ls | awk "{print $2}" | grep -e "${VOLUME}"`
     if [ ! "${VOLUME_EXISTS}" ] ; then
         echo "create volume <${VOLUME}> ..."
-        $SSH sudo /usr/bin/docker volume create --name ${VOLUME}
+        $SSH  /usr/bin/docker volume create --name ${VOLUME}
     fi
 
     EXTRA_OPTIONS=${EXTRA_OPTIONS}" --volume ${VOLUME}:/var/lib/stemmarest/"
@@ -54,7 +54,7 @@ if [ -n "${CONTAINER_ENV}" ]; then
 fi
 
 echo "create new container"
-$SSH sudo /usr/bin/docker create \
+$SSH  /usr/bin/docker create \
   --name    ${INSTANCE_NAME} \
   --memory  ${MEMORY} \
   --cpu-period=100000 \
@@ -63,4 +63,4 @@ $SSH sudo /usr/bin/docker create \
   $EXTRA_OPTIONS \
 ${REGISTRY}/${IMAGE}:${TAG}
 
-$SSH sudo /usr/bin/docker start ${INSTANCE_NAME}
+$SSH  /usr/bin/docker start ${INSTANCE_NAME}
